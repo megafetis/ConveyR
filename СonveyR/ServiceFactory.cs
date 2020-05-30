4c62286fb0ea98f2b8edbc201d5c73596f4f680b
megafetis <megafetis@gmail.com>
2020-05-30 14:26:40 +0300

    add UTypeExtensions lib dependency

--- СonveyR/ServiceFactory.cs
+++ СonveyR/ServiceFactory.cs
@@ -3,6 +3,8 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using UTypeExtensions;
+
 
 namespace ConveyR
 {
@@ -52,56 +54,5 @@ namespace ConveyR
             return handlerType.GetCustomAttribute<ProcessConfigAttribute>() ?? new ProcessConfigAttribute();
         }
 
-        #region InheritsOrImplements
-
-        public static bool InheritsOrImplements(this Type child, Type parent)
-        {
-            parent = ResolveGenericTypeDefinition(parent);
-
-            var currentChild = child.IsGenericType
-                ? child.GetGenericTypeDefinition()
-                : child;
-
-            while (currentChild != typeof(object))
-            {
-                if (parent == currentChild || HasAnyInterfaces(parent, currentChild))
-                    return true;
-
-                currentChild = currentChild.BaseType != null
-                               && currentChild.BaseType.IsGenericType
-                    ? currentChild.BaseType.GetGenericTypeDefinition()
-                    : currentChild.BaseType;
-
-                if (currentChild == null)
-                    return false;
-            }
-            return false;
-        }
-
-        private static Type ResolveGenericTypeDefinition(Type parent)
-        {
-            var shouldUseGenericType = true;
-            if (parent.IsGenericType && parent.GetGenericTypeDefinition() != parent)
-                shouldUseGenericType = false;
-
-            if (parent.IsGenericType && shouldUseGenericType)
-                parent = parent.GetGenericTypeDefinition();
-            return parent;
-        }
-
-        private static bool HasAnyInterfaces(Type parent, Type child)
-        {
-            return child.GetInterfaces()
-                .Any(childInterface =>
-                {
-                    var currentInterface = childInterface.IsGenericType
-                        ? childInterface.GetGenericTypeDefinition()
-                        : childInterface;
-
-                    return currentInterface == parent;
-                });
-        }
-
-        #endregion
     }
 }
